98bf5fbe49758d4d82b5dfb8908daf6d9086589e
arslan <arslanafzal321@gmail.com>
2019-04-10 04:53:16 +0500

    added translation account/logout

--- 3.0.3.1/ur-pk/account/logout.php
+++ 3.0.3.1/ur-pk/account/logout.php
@@ -3,6 +3,6 @@
 $_['heading_title'] = 'اکاوٗنٹ لاگ آوٗٹ';
 
 // Text
-$_['text_message']  = '<p>You have been logged off your account. It is now safe to leave the computer.</p><p>Your shopping cart has been saved, the items inside it will be restored whenever you log back into your account.</p>';
-$_['text_account']  = 'Account';
-$_['text_logout']   = 'Logout';
+$_['text_message']  = '<p>آپ اپنے اکاوٗنٹ سے لاگ آوٗٹ ہو چکے  ہیں۔ اب آپ کمپیوٹر کو چھوڑ کر جا سکتے ہیں۔</p><p>آپ کی خریداری کی ٹوکری محفوظ ہو چکی ہے۔ جب آپ دوبارہ لاگ ان کریں گے تو آپ کی خریداری کی ٹوکری  کی اشیاٗ کو دوبارہ بہال کر دیا جائے گا۔</p>';
+$_['text_account']  = 'اکاوٗنٹ';
+$_['text_logout']   = 'لاگ آوٗٹ';
