86aba13acca89674f9550e2cb371cd51f77c89c5
arslan <arslanafzal321@gmail.com>
2019-04-26 02:54:00 +0500

    added translation account/newsletter

--- 3.0.3.1/ur-pk/account/newsletter.php
+++ 3.0.3.1/ur-pk/account/newsletter.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title']    = 'Newsletter Subscription';
+$_['heading_title']    = 'نیوز لیٹر سبسکرائب کریں';
 
 // Text
-$_['text_account']     = 'Account';
-$_['text_newsletter']  = 'Newsletter';
-$_['text_success']     = 'Success: Your newsletter subscription has been successfully updated!';
+$_['text_account']     = 'اکاوٗنٹ';
+$_['text_newsletter']  = 'نیوز لیٹر';
+$_['text_success']     = 'آپ کی نیوز لیٹر کو سبسکراسبسکرائب کرنے کی تفصیلات کامیابی سے تبدیل ہو چکی ہیں';
 
 // Entry
-$_['entry_newsletter'] = 'Subscribe';
+$_['entry_newsletter'] = 'سبسکرائب';
